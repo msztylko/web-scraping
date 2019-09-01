79e3b6fb358ea77f64738cf9232b81cb7a3b8d7d
Marcin <marcin.sztylko@gmail.com>
2019-09-01 17:25:39 +0200

    working version

--- SA_scraper.py
+++ SA_scraper.py
@@ -1,12 +1,14 @@
-import requests, os, bs4
+import requests, os
 
 url = 'http://blog.samaltman.com'
-os.makedirs('samaltman', exist_ok=True)
+os.makedirs('sam-altman', exist_ok=True) #create directory for saving all fav blog posts
+path = os.path.join(os.getcwd(), 'sam-altman')
 
 fav_urls = set()
 with open('fav_blog_posts.txt') as file:
     lines = file.readlines()
     for line in lines:
+        #remove ? , - ' from url
         line = line.lower().replace('?', '').replace(',', '').replace('-', '').replace("'", "").split()
         fav_url = '-'.join(line)
         fav_urls.add(fav_url)
@@ -14,16 +16,21 @@ with open('fav_blog_posts.txt') as file:
 error_count = 0
 downloaded = 0
 for fav_url in fav_urls:
-    print('Downloading webpage {}'.format(url + '/' + fav_url))
-    res = requests.get(url + '/' + fav_url)
+    full_url = url + '/' + fav_url
+    print('Accessing webpage {}'.format(full_url))
+    res = requests.get(full_url)
     try:
         res.raise_for_status()
+        file_name = os.path.join(path, fav_url + ".html")
+        with open(file_name, 'wb') as file:
+            for chunk in res.iter_content(100000):
+                file.write(chunk)
+                print("Succesfully downloaded {}".format(fav_url))
         downloaded += 1
     except Exception as exc:
         print('Problem {}'.format(exc))
         error_count += 1
+    print()
 
 print("Succesfully downloaded {} blog posts and "
       "encountered problems with {} blog posts.". format(downloaded, error_count))
-
-
