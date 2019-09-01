cfaa02ada5bda47528e5e4f9dcdfc24f736e27ce
Marcin <marcin.sztylko@gmail.com>
2019-09-01 16:31:06 +0200

    loop for getting every fav blog post

--- SA_scraper.py
+++ SA_scraper.py
@@ -7,26 +7,23 @@ fav_urls = set()
 with open('fav_blog_posts.txt') as file:
     lines = file.readlines()
     for line in lines:
-        line = line.lower().replace('?', '').replace(',', '').replace('-', '').replace("'", "")
-        line = line.split()
+        line = line.lower().replace('?', '').replace(',', '').replace('-', '').replace("'", "").split()
         fav_url = '-'.join(line)
         fav_urls.add(fav_url)
 
+error_count = 0
+downloaded = 0
+for fav_url in fav_urls:
+    print('Downloading webpage {}'.format(url + '/' + fav_url))
+    res = requests.get(url + '/' + fav_url)
+    try:
+        res.raise_for_status()
+        downloaded += 1
+    except Exception as exc:
+        print('Problem {}'.format(exc))
+        error_count += 1
 
-# for url in fav_urls:
-#     print('Downloading webpage {}'.format(url))
-#     res = requests.get(url)
+print("Succesfully downloaded {} blog posts and "
+      "encountered problems with {} blog posts.". format(downloaded, error_count))
 
 
-# try:
-#     res.raise_for_status()
-# except Exception as exc:
-#     print('Problem {}'.format(exc))
-#
-# resSoup = bs4.BeautifulSoup(res.text)
-#
-# file = open('Sam_Altman.txt', 'wb')
-# for chunk in res.iter_content(100000):
-#     file.write(chunk)
-#
-# file.close
